e4fdf41d7885f882529f9ae0ec2734955d363e38
Thailon Dorneles <thailon74@gmail.com>
2026-07-16 20:16:08 +0400

    Limpeza do código e exclusão de arquivos,serviços e bibliotecas que não são mais utillizados

--- src/services/estoqueDisponivel.js
+++
@@ -1,7 +0,0 @@
-import { unimedApi } from "../config/apis.js";
-
-export function getEstoqueDisponivel({ codItem, codUnidade, offset = 0, limit = 25 }) {
-    return unimedApi.get(`estoqueDisponivel/${codItem}/${codUnidade}`, {
-        params: { offset, limit }
-    });
-}
